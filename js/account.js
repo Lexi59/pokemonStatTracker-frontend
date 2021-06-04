23b53c6a50b91f60f78d9a14ca786f0a4aa48228
Lexi59 <trip9919@gmail.com>
2021-06-04 10:25:34 -0500

    adding change password

--- js/account.js
+++ js/account.js
@@ -14,3 +14,73 @@ function checkUsername(){
         }
     });
 }
+
+document.querySelector('#accountForm').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    if(e.submitter.id == 'changePassBtn'){
+        var elems = document.querySelectorAll(".hidden");
+
+        [].forEach.call(elems, function(el) {
+            el.classList.remove("hidden");
+        });
+    
+        document.querySelector('#changePassBtn').classList.add('hidden');
+    }
+    else{
+        clearErrorMessages();
+        //submit new password
+        const username = document.querySelector('#username').value.trim();
+        const newPassword = document.querySelector('#newPassword').value.trim();
+        const password = document.querySelector('#password').value.trim();
+        const confirmPassword = document.querySelector('#confirmNewPassword').value.trim();
+        var error = false;
+        
+        if(newPassword != confirmPassword){
+            logErrorMessage("Passwords must match");
+            document.querySelector('#newPassword').classList.add('is-invalid');
+            document.querySelector('#confirmNewPassword').classList.add('is-invalid');
+            error = true;
+        }
+        if(newPassword.length < 6){
+            logErrorMessage("Invalid Password");
+            document.querySelector('#newPassword').classList.add('is-invalid');
+            error = true;
+        }
+        if(!error){
+            //valid user account
+            var user = {
+                username,
+                password,
+                newPassword
+            };
+            fetch(API_URL+'auth/updateUser',{
+                method: 'POST',
+                body: JSON.stringify(user),
+                headers: {
+                    'content-type':'application/json'
+                }
+            }).then((response) => {
+                if(response.ok){return response.json();}
+                    throw response;
+            }).then((result)=>{
+                localStorage.token = result.token;
+                window.location.replace("../pages/dashboard.html");
+            }).catch((error)=>{
+                error.text().then(msg =>{
+                    logErrorMessage(JSON.parse(msg).message);
+                    console.error(JSON.parse(msg).message);
+                });
+            });
+        }
+    }
+
+});
+
+function logErrorMessage(msg){
+    var div = document.createElement('div');
+    div.innerHTML = `<div class="alert alert-danger">`+msg+`</div>`;
+    document.querySelector('#error').appendChild(div);
+}
+function clearErrorMessages(){
+    document.querySelector('#error').innerHTML="";
+}
